2bf8337eea844b480a80056410d843b4162d42e6
Markus Mertama <mertama@gmail.com>
2020-12-08 20:32:32 +0200

    Update setup.py

--- affiliates/py_client/setup.py
+++ affiliates/py_client/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='py_client',
     version='',
+    py_modules=['pyclient'],
     install_requires=['webview', 'websockets'],
     url='https://github.com/mmertama/Gempyre',
     license='',
