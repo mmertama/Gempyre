c69b7d4cfc464bc298bdc0c3164442dbe2613f66
Markus Mertama <markus.mertama@intopalo.com>
2021-01-08 01:03:26 +0200

    fixes for rpc

--- affiliates/py_uisrv/ui_srvclient.py
+++ affiliates/py_uisrv/ui_srvclient.py
@@ -3,19 +3,25 @@ import socket
 import re
 
 
-def main():
-    host = re.search(r'host=(\S+)', ' '.join(sys.argv))[1]
-    port = int(re.search(r'port=(\S+)', ' '.join(sys.argv))[1])
-    command = [x for x in sys.argv if not re.match(r'(port|host)=', x) and sys.argv.index(x) > 0]
+def main(params):
+    host = re.search(r'host=(\S+)', ' '.join(params))[1]
+    port = int(re.search(r'port=(\S+)', ' '.join(params))[1])
+    commands = [x for x in params if not re.match(r'(port|host)=', x) and params.index(x) > 0]
+    url = commands[0]
+    addr = commands[1]
+    remote_url = re.sub(r'localhost', addr, url)
+    commands = [remote_url] + commands[2:]
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         sock.connect((host, port))
     except ConnectionRefusedError as cre:
         print("Cannot connect to", host, port, cre)
         return -2;
-    encoded = str.encode(' '.join(command))
-    sock.send(len(encoded).to_bytes(8, byteorder='big'))
-    sock.send(encoded)
+    sock.send(len(commands).to_bytes(8, byteorder='big'))
+    for command in commands:
+        encoded = str.encode(command)
+        sock.send(len(encoded).to_bytes(8, byteorder='big'))
+        sock.send(encoded)
     return_status = None
     try:
         return_status = sock.recv(8)
@@ -26,5 +32,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
 
