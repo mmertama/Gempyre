511b15276c9af8af92aad9a0203503590b266449
Markus Mertama <mertama@gmail.com>
2021-01-10 00:29:29 +0200

    add filemap

--- scripts/genStringResource.py
+++ scripts/genStringResource.py
@@ -4,7 +4,6 @@ import re
 import os
 import argparse
 
-
 app = os.path.abspath(sys.argv[0])
 appdir = os.path.dirname(app)
 
@@ -23,14 +22,17 @@ outName = args.target
 directory = os.path.dirname(outName)
 if not os.path.exists(directory):
     os.makedirs(directory)
-        
+
 with open(outName, 'w') as file:
     cname = re.sub('[^a-zA-Z_]', '_', os.path.basename(outName)).upper()
     file.write('#ifndef ' + cname + '\n')
     file.write('#define ' + cname + '\n')
     file.write('#include <string>\n')
-    
-    for inName in args.sources:  
+    file.write('#include <unordered_map>\n')
+
+    item_list = {}
+
+    for inName in args.sources:
         domin = True if args.minify and os.path.splitext(inName) == 'js' else False
 
         encoded = None
@@ -39,17 +41,24 @@ with open(outName, 'w') as file:
             content = jsmin(infile.read()) if domin else infile.read()
             encoded = base64.standard_b64encode(content).decode('utf-8')
 
-    
         sname = re.sub('[^a-zA-Z_]', '', os.path.basename(inName)).capitalize()
 
-        chunksize = 1024 
-        data = [encoded [ i - chunksize : i ] for i in range(chunksize, len(encoded) + chunksize , chunksize)]  
+        item_list[os.path.basename(inName)] = sname
 
-        file.write('const std::string ' + sname + '=\nstd::string("')
+        chunksize = 1024
+        data = [encoded[i - chunksize: i] for i in range(chunksize, len(encoded) + chunksize, chunksize)]
+
+        file.write('const std::string ' + sname + ' =\nstd::string("')
         file.write('") +\nstd::string("'.join(data))
         file.write('");\n')
-        
-    file.write('#endif //' + cname +  '\n')
+
+    list_name = re.sub('[^a-zA-Z_]', '', os.path.basename(outName)).capitalize()
+    file.write('const std::unordered_map<std::string, const std::string_view> ' + list_name + '{\n')
+    for k, v in item_list.items():
+        file.write('\t{"/' + k + '", ' + v + '},\n')
+    file.write('};\n')
+
+    file.write('#endif //' + cname + '\n')
 
 
 
