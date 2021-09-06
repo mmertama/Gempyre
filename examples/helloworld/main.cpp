641186bd9225251184ed775b5a0544f45132e97a
Markus Mertama <mertama@gmail.com>
2021-09-06 23:38:41 +0300

    setTitle and resize

--- examples/helloworld/main.cpp
+++ examples/helloworld/main.cpp
@@ -1,38 +1,79 @@
 #include "gempyre.h"
 #include "gempyre_utils.h"
 #include <iostream>
+#include <cassert>
 
 const unsigned short DefaultPort = 8080;
-
+#include "html_resource.h"
 
 int main(int argc, char** argv) {
     Gempyre::setDebug();
     const auto p = GempyreUtils::parseArgs(argc, argv, {{"port", 'p', GempyreUtils::ArgType::REQ_ARG}});
+
+    std::function<std::unique_ptr<Gempyre::Ui> ()> makeUi = nullptr;
+
     if(std::get<GempyreUtils::ParamList>(p).size() < 1) {
-        std::cerr << "[path to INDEX.HTML] <-p value>" << std::endl;
-        return -1;
+        makeUi = [&]() {
+            return std::make_unique<Gempyre::Ui>(Html_resourceh, "index.html", argc, argv);
+        };
+
+    } else {
+        makeUi = [&]() {
+            const auto indexPath = std::get<GempyreUtils::ParamList>(p)[0];
+            Gempyre::Ui::Filemap map;
+            const auto url = Gempyre::Ui::addFile(map, indexPath);
+            gempyre_utils_assert_x(url, "Not Found:" + indexPath);
+            return std::make_unique<Gempyre::Ui>(map, *url, argc, argv, "",
+                         GempyreUtils::to<unsigned short>(GempyreUtils::atOr(std::get<GempyreUtils::Options>(p), "port", std::to_string(DefaultPort))),
+                         GempyreUtils::pathPop(indexPath)); //root directory;
+        };
     }
-    const auto indexPath = std::get<GempyreUtils::ParamList>(p)[0];
-    Gempyre::Ui::Filemap map;
-    const auto url = Gempyre::Ui::addFile(map, indexPath);
-    gempyre_utils_assert_x(url, "Not Found:" + indexPath);
-    Gempyre::Ui ui(map, *url,
-                 GempyreUtils::to<unsigned short>(GempyreUtils::atOr(std::get<GempyreUtils::Options>(p), "port", std::to_string(DefaultPort))),
-                 GempyreUtils::pathPop(indexPath)); //root directory;
-    Gempyre::Element text(ui, "content");
-    Gempyre::Element button(ui, "startbutton");
+
+    auto ui = makeUi();
+
+    Gempyre::Element text(*ui, "content");
+    Gempyre::Element button(*ui, "startbutton");
     button.setHTML("Hello?");
-    button.subscribe("click", [&ui, &text](const Gempyre::Event&) {
+    button.subscribe("click", [&ui, &text](auto) {
         text.setHTML("Hello World!");
-        const auto p = ui.ping();
+        const auto p = ui->ping();
         if(p.has_value()) {
             auto [ping, halfping] = p.value();
             text.setHTML("Ping roundtrip:" + std::to_string(static_cast<double>(ping.count()) / 1000.)
                     + " to UI:" + std::to_string(static_cast<double>(halfping.count()) / 1000.));
        }
-
     });
-    ui.run();
+
+    Gempyre::Element(*ui, "title").subscribe("change", [&ui](const auto& event)  {
+        const auto title_name = event.properties.at("value");
+        ui->setTitle(title_name);
+    }, {"value"});
+
+            ui->onOpen([&ui] {
+               const auto r = ui->root().rect();
+               if(r) {
+                   Gempyre::Element(*ui, "width").setAttribute("value", std::to_string(r->width));
+                   Gempyre::Element(*ui, "height").setAttribute("value", std::to_string(r->height));
+               }
+            });
+
+    Gempyre::Element(*ui, "width").subscribe("change", [&ui](const auto& event)  {
+        const auto w = GempyreUtils::to<int>(event.properties.at("value"));
+        const auto r = ui->root().rect();
+        assert(r);
+        ui->resize(w, r->height);
+    }, {"value"});
+
+
+    Gempyre::Element(*ui, "height").subscribe("change", [&ui](const auto& event)  {
+        const auto h = GempyreUtils::to<int>(event.properties.at("value"));
+        const auto r = ui->root().rect();
+        assert(r);
+        ui->resize(r->width, h);
+    }, {"value"});
+
+
+    ui->run();
     return 0;
 }
 
--- scripts/genStringResource.py
+++ scripts/genStringResource.py
@@ -4,62 +4,62 @@ import re
 import os
 import argparse
 
-app = os.path.abspath(sys.argv[0])
-appdir = os.path.dirname(app)
+def main():
+    app = os.path.abspath(sys.argv[0])
+    appdir = os.path.dirname(app)
 
-sys.path.append(appdir + '/jsmin-2.2.2')
-from jsmin import jsmin
+    sys.path.append(appdir + '/jsmin-2.2.2')
+    from jsmin import jsmin
 
-parser = argparse.ArgumentParser()
-parser.add_argument('target')
-parser.add_argument('sources', nargs='+')
-parser.add_argument('-m', '--minify', dest='minify')
-args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('target')
+    parser.add_argument('sources', nargs='+')
+    parser.add_argument('-m', '--minify', dest='minify')
+    args = parser.parse_args()
 
-print("GSR", args.target, args.sources)
+    outName = args.target
+    directory =  os.path.dirname(outName) if '/' in outName else "."
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-outName = args.target
-directory = os.path.dirname(outName)
-if not os.path.exists(directory):
-    os.makedirs(directory)
+    with open(outName, 'w') as file:
+        cname = re.sub('[^a-zA-Z_]', '_', os.path.basename(outName)).upper()
+        file.write('#ifndef ' + cname + '\n')
+        file.write('#define ' + cname + '\n')
+        file.write('#include <string>\n')
+        file.write('#include <unordered_map>\n')
 
-with open(outName, 'w') as file:
-    cname = re.sub('[^a-zA-Z_]', '_', os.path.basename(outName)).upper()
-    file.write('#ifndef ' + cname + '\n')
-    file.write('#define ' + cname + '\n')
-    file.write('#include <string>\n')
-    file.write('#include <unordered_map>\n')
+        item_list = {}
 
-    item_list = {}
+        for inName in args.sources:
+            domin = True if args.minify and os.path.splitext(inName) == 'js' else False
 
-    for inName in args.sources:
-        domin = True if args.minify and os.path.splitext(inName) == 'js' else False
+            encoded = None
 
-        encoded = None
+            with open(inName, 'rb') as infile:
+                content = jsmin(infile.read()) if domin else infile.read()
+                encoded = base64.standard_b64encode(content).decode('utf-8')
 
-        with open(inName, 'rb') as infile:
-            content = jsmin(infile.read()) if domin else infile.read()
-            encoded = base64.standard_b64encode(content).decode('utf-8')
+            sname = re.sub('[^0-9a-zA-Z_]', '', os.path.basename(inName)).capitalize()
 
-        sname = re.sub('[^0-9a-zA-Z_]', '', os.path.basename(inName)).capitalize()
+            item_list[os.path.basename(inName)] = sname
 
-        item_list[os.path.basename(inName)] = sname
+            chunksize = 1024
+            data = [encoded[i - chunksize: i] for i in range(chunksize, len(encoded) + chunksize, chunksize)]
 
-        chunksize = 1024
-        data = [encoded[i - chunksize: i] for i in range(chunksize, len(encoded) + chunksize, chunksize)]
+            file.write('const std::string ' + sname + ' =\nstd::string("')
+            file.write('") +\nstd::string("'.join(data))
+            file.write('");\n')
 
-        file.write('const std::string ' + sname + ' =\nstd::string("')
-        file.write('") +\nstd::string("'.join(data))
-        file.write('");\n')
+        list_name = re.sub('[^a-zA-Z_]', '', os.path.basename(outName)).capitalize()
+        file.write('const std::unordered_map<std::string, std::string> ' + list_name + '{\n')
+        for k, v in item_list.items():
+            file.write('\t{"/' + k + '", ' + v + '},\n')
+        file.write('};\n')
 
-    list_name = re.sub('[^a-zA-Z_]', '', os.path.basename(outName)).capitalize()
-    file.write('const std::unordered_map<std::string, std::string> ' + list_name + '{\n')
-    for k, v in item_list.items():
-        file.write('\t{"/' + k + '", ' + v + '},\n')
-    file.write('};\n')
-
-    file.write('#endif //' + cname + '\n')
-
+        file.write('#endif //' + cname + '\n')
 
 
+if __name__ == "__main__":
+    main()
 
