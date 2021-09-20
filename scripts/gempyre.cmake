8c5fe154e0ceac0670ce21c9eab3aaef2eca7eb2
Markus Mertama <markus.mertama@intopalo.com>
2021-09-20 23:10:43 +0300

    gen resource script shall use find_file

--- scripts/gempyre.cmake
+++ scripts/gempyre.cmake
@@ -1,24 +1,11 @@
+set(GEMPYRE_FUNCTION_DIR ${CMAKE_CURRENT_LIST_DIR})
 function (gempyre_add_resources)
     cmake_parse_arguments(
         ADD_RESOURCE
         "MINIFY" "TARGET;PROJECT" "SOURCES"
         ${ARGN}
         )
-    
-    if(NOT EXISTS ${GEMPYRE_DIR})
-        if(EXISTS $ENV{GEMPYRE_DIR})
-            set(GEMPYRE_DIR $ENV{GEMPYRE_DIR})
-        elseif(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/gempyrelib)
-            set(GEMPYRE_DIR ${CMAKE_CURRENT_SOURCE_DIR})
-        elseif(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/../gempyrelib)
-               set(GEMPYRE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/..)
-        elseif(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/../Gempyre/gempyrelib)
-               set(GEMPYRE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../Gempyre)
-	else()
-            message(FATAL_ERROR "GEMPYRE_DIR is not set neither GEMPYRE_DIR Environment variable is not pointing to Gempyre dir (current working dir: ${CMAKE_CURRENT_SOURCE_DIR})")
-        endif()
-    endif()
-    
+
     if({ADD_RESOUCE_MINIFY})
         set(MINIFY "--minify")
     else()
@@ -31,11 +18,17 @@ function (gempyre_add_resources)
 	else()
 		find_program(PYTHON "python" REQUIRED)
 		set (PYTHON3 python)
-	endif()
+        endif()
+
+    find_file(GEN_RESOURCE
+        genStringResource.py
+        PATHS "${GEMPYRE_FUNCTION_DIR}" "${CMAKE_SOURCE_DIR}/scripts" "${CMAKE_CURRENT_LIST_DIR}"
+        REQUIRED
+        )
       
     add_custom_command(
         OUTPUT ${ADD_RESOURCE_TARGET}
-        COMMAND ${PYTHON3} ${GEMPYRE_DIR}/scripts/genStringResource.py ${MINIFY} ${ADD_RESOURCE_TARGET} ${ADD_RESOURCE_SOURCES}
+        COMMAND ${PYTHON3} ${GEN_RESOURCE} ${MINIFY} ${ADD_RESOURCE_TARGET} ${ADD_RESOURCE_SOURCES}
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         )
     
