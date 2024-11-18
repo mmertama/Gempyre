c6053599a27fbcdf8e833208fc93232416b71df7
Markus Mertama <mertama@gmail.com>
2024-11-18 16:22:35 +0200

    supressing libwebsocket build warnings

--- gempyrelib/cmake/libwebsockets.cmake
+++ gempyrelib/cmake/libwebsockets.cmake
@@ -5,6 +5,8 @@ else()
     set(LIB_WS_VER "v4.3.3")
 endif()
 
+set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 1 CACHE INTERNAL "Suppress developer warnings")
+
 # this was GIT_REPOSITORY https://libwebsockets.org/repo/libwebsockets
 # but the respository seems not be that responding and hence changed
 # to github
@@ -30,42 +32,54 @@ if(WIN32)
   set(LWS_WITH_SYS_SMD OFF)
   set(LWS_EXT_PTHREAD_LIBRARIES "")
   set(LWS_STATIC_PIC  ON)
-    set(LWS_INSTALL_LIB_DIR ${CMAKE_BINARY_DIR}/foo1)
-      set(LWS_INSTALL_CMAKE_DIR ${CMAKE_BINARY_DIR}/foo2)
-      set(LWS_INSTALL_BIN_DIR ${CMAKE_BINARY_DIR}/foo3)
-      set(LWS_INSTALL_EXAMPLES_DIR ${CMAKE_BINARY_DIR}/foo5)
-  endif()
+  set(LWS_INSTALL_LIB_DIR ${CMAKE_BINARY_DIR}/foo1)
+  set(LWS_INSTALL_CMAKE_DIR ${CMAKE_BINARY_DIR}/foo2)
+  set(LWS_INSTALL_BIN_DIR ${CMAKE_BINARY_DIR}/foo3)
+  set(LWS_INSTALL_EXAMPLES_DIR ${CMAKE_BINARY_DIR}/foo5)
+endif()
 
-  cmake_policy(SET CMP0077 NEW)
+cmake_policy(SET CMP0077 NEW)
 
-  set(LWS_WITHOUT_TESTAPPS ON CACHE BOOL "" FORCE)
-  set(LWS_WITH_MINIMAL_EXAMPLES OFF CACHE BOOL "" FORCE)
-  set(LWS_WITH_STATIC ON CACHE BOOL "" FORCE)
-  set(LWS_WITH_SHARED OFF CACHE BOOL "" FORCE)
-  set(LWS_WITH_SSL OFF CACHE BOOL "" FORCE)
-  set(LWS_ROLE_H1 ON CACHE BOOL "" FORCE)
-  set(LWS_ROLE_H2 ON CACHE BOOL "" FORCE)
-  set(LWS_WITH_HTTP2 ON CACHE BOOL "" FORCE)
-  set(LWS_ROLE_WS ON CACHE BOOL "" FORCE)
-  set(LWS_CTEST_INTERNET_AVAILABLE OFF CACHE BOOL "" FORCE)
+set(LWS_WITHOUT_TESTAPPS ON CACHE BOOL "" FORCE)
+set(LWS_WITH_MINIMAL_EXAMPLES OFF CACHE BOOL "" FORCE)
+set(LWS_WITH_STATIC ON CACHE BOOL "" FORCE)
+set(LWS_WITH_SHARED OFF CACHE BOOL "" FORCE)
+set(LWS_WITH_SSL OFF CACHE BOOL "" FORCE)
+set(LWS_ROLE_H1 ON CACHE BOOL "" FORCE)
+set(LWS_ROLE_H2 ON CACHE BOOL "" FORCE)
+set(LWS_WITH_HTTP2 ON CACHE BOOL "" FORCE)
+set(LWS_ROLE_WS ON CACHE BOOL "" FORCE)
+set(LWS_CTEST_INTERNET_AVAILABLE OFF CACHE BOOL "" FORCE)
 
   
-  # Windows
-  set(LWS_SSL_CLIENT_USE_OS_CA_CERTS OFF)
+# Windows
+set(LWS_SSL_CLIENT_USE_OS_CA_CERTS OFF)
 
-  set(SOCKETS_LIB websockets)
+set(SOCKETS_LIB websockets)
 
   # install
-  FetchContent_GetProperties(libwebsockets)
-  if(NOT libwebsockets_POPULATED)
-    FetchContent_Populate(libwebsockets)
-    add_subdirectory(${libwebsockets_SOURCE_DIR} ${libwebsockets_BINARY_DIR})
-  endif()
+FetchContent_GetProperties(libwebsockets)
+if(NOT libwebsockets_POPULATED)
+  FetchContent_Populate(libwebsockets)
+  add_subdirectory(${libwebsockets_SOURCE_DIR} ${libwebsockets_BINARY_DIR})
+endif()
 
-  include_directories(${libwebsockets_SOURCE_DIR}/include ${libwebsockets_BINARY_DIR})
-  set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH};${libwebsockets_SOURCE_DIR}/cmake")
+include_directories(${libwebsockets_SOURCE_DIR}/include ${libwebsockets_BINARY_DIR})
+set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH};${libwebsockets_SOURCE_DIR}/cmake")
 
-  #set(WS_LIBS websockets ${LIBWEBSOCKETS_DEP_LIBS})
+#set(WS_LIBS websockets ${LIBWEBSOCKETS_DEP_LIBS})
+
+#target_compile_options(libwebsockets PRIVATE -Wno-unused-parameter)
+
+if (MSVC)
+  target_compile_options(websockets PRIVATE /wd5102 /wd4005)
+else()
+  target_compile_options(websockets PRIVATE -Wno-unused-parameter)
+endif()
+
+if (MINGW)
+  target_compile_options(websockets PRIVATE -Wno-error=macro-redefined)
+endif()
 
 set(USE_LIBWEBSOCKETS TRUE)
 
@@ -74,13 +88,17 @@ macro(socket_dependencies TARGET)
     target_compile_definitions(${TARGET} PRIVATE USE_LIBWEBSOCKETS)
 endmacro()
 
-set(UNUSED_PARAMETERS
-  "${libwebsockets_SOURCE_DIR}/lib/plat/unix/unix-misc.c"
-  "${libwebsockets_SOURCE_DIR}/lib/plat/unix/unix-sockets.c"
-  "${libwebsockets_SOURCE_DIR}/lib/plat/unix/unix-init.c" 
-)
-
-supress_errors(FILES ${UNUSED_PARAMETERS} FLAGS "-Wno-unused-parameter")
+#set(SUPPRESS_FILES
+#  "${libwebsockets_SOURCE_DIR}/lib/plat/unix/unix-misc.c"
+#  "${libwebsockets_SOURCE_DIR}/lib/plat/unix/unix-sockets.c"
+#  "${libwebsockets_SOURCE_DIR}/lib/plat/unix/unix-init.c"
+#  "${libwebsockets_SOURCE_DIR}/lib/roles/ws/ops-ws.c"
+#  "${libwebsockets_SOURCE_DIR}/lib/roles/listen/ops-listen.c"
+#  "${libwebsockets_SOURCE_DIR}/lib/roles/raw-file/ops-raw-file.c"
+#  "${libwebsockets_SOURCE_DIR}/lib/roles/h2/ops-h2.c"    
+#)
+#
+#suppress_errors(FILES ${SUPPRESS_FILES} FLAGS "-Wno-unused-parameter" "-Wno-dev")
 
 string(STRIP "${libwebsockets_BINARY_DIR}/lib" GEMPYRE_WS_LIB_PATH)
 
